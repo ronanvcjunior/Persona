6bf8acf32fa9f0faaad19f72cab74acacc5b56ff
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2021-04-14 15:49:28 -0300

    persona

--- js/js-persona.js
+++ js/js-persona.js
@@ -31,7 +31,6 @@ function openPersona(persona) {
 	document.getElementById('personaFoto').src = foto
 
 	if (persona === 'ricardo') {
-		document.getElementById('personaCircle').style.background = '#379bf7'
 		document.getElementById('divisao').style.background = '#379bf7'
 		document.getElementById('personaResumo').style.background = '#379bf7'
 
@@ -40,8 +39,9 @@ function openPersona(persona) {
 		document.getElementById('idade').innerHTML = '20 anos'
 		document.getElementById('cidade').innerHTML = 'São Luís do Maranhão'
 		document.getElementById('cursa').innerHTML = 'Engenharia da Computação na UFG'
+
+		document.getElementById('paragrafo1').innerHTML = 'A rotina de Ricardo consiste em organizar suas tarefas pela manhã, fazer estágio durante a tarde, estudar no período da noite e jogar durante a madrugada, não costuma praticar esportes. Ricardo é um menino eclético, gosta de ouvir funk, eletrônica e rock. Em seu tempo livre gosta de assistir desenhos, seu preferido é O Incrível Mundo de Gumball. Também gosta de jogos online e ver vídeos na internet, principalmente stream de jogos, seu hobby favorito no momento é Genshin. Suas redes sociais mais usadas são twitter, whatsapp, reddit e instagram. Costuma se informar por leitura de apostilas, artigos, livros, jornais, reddit e pela internet no geral.' + '<br>' + 'O estudante é um menino que gosta de rir e se divertir, é paciente, persistente e tímido. Para o futuro, quer se formar e ter um bom emprego, que seja confortável e não cansativo, também deseja se casar, tornar-se mais confiante e estabilizado. Ricardo enfrenta o medo da pandemia, a ansiedade de não conseguir se formar no tempo certo e de haver muita concorrência no mercado de trabalho.' + '<br>' + 'O jovem tem interesse em sites que delegam estágio, inclusive já usou alguns, tem preferência por sites com informações claras e sucintas, que sejam de fácil acesso e uso, sempre espera encontrar várias oportunidades de trabalho.'
 	} else if (persona === 'nicole') {
-		document.getElementById('personaCircle').style.background = '#9bdc28'
 		document.getElementById('divisao').style.background = '#9bdc28'
 		document.getElementById('personaResumo').style.background = '#9bdc28'
 
@@ -50,5 +50,7 @@ function openPersona(persona) {
 		document.getElementById('idade').innerHTML = '20 anos'
 		document.getElementById('cidade').innerHTML = 'Sanclerlândia de Goiás'
 		document.getElementById('cursa').innerHTML = 'Design Gráfico na UFG'
+
+		document.getElementById('paragrafo1').innerHTML = 'Nicole começa seu dia trabalhando, à tarde faz faculdade, logo após faz uma caminhada ou vai para academia e termina o dia assistindo um filme. A estudante gosta de ouvir funk, sertanejo e pop, em seu tempo livre gosta de assistir filmes de terror e beber uma cerveja. Suas redes sociais mais usadas são o Pinterest e Instagram, onde busca referências de design e compartilha seus trabalhos, além disso, usa muito o WhatsApp para se comunicar. Busca informações principalmente em livros e redes sociais, como facebook e instagram, através de referências que conheceu na faculdade.' + '<br>' + 'Nicole é uma pessoa ansiosa, alegre e engraçada. Faz serviço voluntário em uma Empresa Júnior (EJ), onde é gerente de projetos. Apesar de não estabelecer planos para o futuro, planeja terminar o curso e ter um bom serviço, ser uma pessoa mais confiante, organizada e independente financeiramente, também quer constituir uma família. Seus maiores medos são de não conseguir terminar a faculdade no tempo certo e não ser feliz na sua profissão. Nicole nunca acessou sites de delegação de estágio, mas vê com bons olhos começar a utilizá-los, desde que tenha um sistema de busca eficaz, que organize os estágios por áreas de atuação, e que seja um site esteticamente agradável e seguro.'
 	}
 }
